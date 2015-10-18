8a8be52d8595a51be7fd1de10be96f8656370e38
XperianPro <karlo.puselj@gmail.com>
2015-10-18 17:38:37 +0200

    ažuriranje login podataka i ip adrese servera

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function db_execute( $sql )
 {
   try {
-    $baza = new PDO( "mysql:host=localhost;dbname=shop", "root", "");
+    $baza = new PDO( "mysql:host=eu-cdbr-azure-west-c.cloudapp.net;dbname=acsm_d911235063031c3", "b01917dcc7cc15", "9d201e27");
   } catch (PDOException $e) {
     echo 'Connection failed: '.$e->getMessage();
   }
@@ -13,7 +13,7 @@ function db_execute( $sql )
 function db_query( $sql )
 {
   try {
-    $baza = new PDO( "mysql:host=localhost;dbname=shop", "root", "");
+    $baza = new PDO( "mysql:host=eu-cdbr-azure-west-c.cloudapp.net;dbname=acsm_d911235063031c3", "b01917dcc7cc15", "9d201e27");
   } catch (PDOException $e) {
     echo 'Connection failed: '.$e->getMessage();
   }
